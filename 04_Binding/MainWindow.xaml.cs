ba23a887900f1ad144827b3f2f834a80424f7d90
TBabych <t.iu.babych@nuwm.edu.ua>
2022-12-21 19:35:25 +0200

    add binding

---
+++ 04_Binding/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public User AdminUser { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            AdminUser = new User()
+            {
+                Email = "example@example.com",
+                AuthData = new AuthData()
+                {
+                    Login = "admin007",
+                    Password = "qwerty007"
+                },
+                Age=19
+            };
+
+            this.DataContext = AdminUser;
+
+        }
+
+        public struct AuthData{
+            public string Login { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class User { 
+            public string Email { get; set; }
+            public AuthData AuthData { get; set; }
+            public int Age { get; set; }
+             public override string ToString()
+            {
+                return $"User: {Email}, {AuthData.Login}\n"
+                         +$"Password: {AuthData.Password.First()}{new string('#', AuthData.Password.Length-2)}{AuthData.Password.Last()}  " ;
+            }
+        }
+    }
+}
---
+++ IntroWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var submitButton = new Button
+            //{
+            //    Content = "Button2",
+            //    FontFamily=new FontFamily("Consolas"),
+            //    FontSize=20.0,
+            //    Foreground=Brushes.Green
+
+            //};
+            //Wrapper.Children.Add(submitButton);
+         
+        }
+    }
+}
